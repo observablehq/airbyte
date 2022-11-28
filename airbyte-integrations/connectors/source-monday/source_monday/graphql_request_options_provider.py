fce7022c1c5aa567050b53e13b28f92cc034b5d9
Roman Yermilov <roman.yermilov@globallogic.com>
2022-11-28 22:47:42 +0400

    Source Monday: fix tests and small code fixes and docstrig

--- airbyte-integrations/connectors/source-monday/source_monday/graphql_request_options_provider.py
+++ airbyte-integrations/connectors/source-monday/source_monday/graphql_request_options_provider.py
@@ -27,10 +27,12 @@ class GraphQLRequestOptionsProvider(InterpolatedRequestOptionsProvider):
             return schema_dict["properties"]
 
     def _get_query_by_stream_properties(self, props: dict):
+        # Special debug code needed for tests to pass successfully because of rate limits.
+        # It makes a query cost less points and should not be used to production
         if self.name == "teams" and self.config.get("debug"):
             return "id,name,picture_url,users(limit:100){id}"
-        query = ','.join([f'{col}' + (f'{{{self._get_query_by_stream_properties(props[col]["properties"])}}}' if 'properties' in props[col] else '') for col in props])
-        return query
+
+        return ','.join([f'{col}' + (f'{{{self._get_query_by_stream_properties(props[col]["properties"])}}}' if 'properties' in props[col] else '') for col in props])
 
     def get_request_params(self, *, stream_state: Optional[StreamState] = None, stream_slice: Optional[StreamSlice] = None,
                            next_page_token: Optional[Mapping[str, Any]] = None) -> MutableMapping[str, Any]:
@@ -42,9 +44,11 @@ class GraphQLRequestOptionsProvider(InterpolatedRequestOptionsProvider):
             graphql_params["page"] = next_page_token[self.NEXT_PAGE_TOKEN_FIELD_NAME]
 
         graphql_query = ",".join([f"{k}:{v}" for k, v in graphql_params.items()])
-        # Monday uses a query string to pass in environments
+
+        # Special debug code needed for tests to pass successfully because of rate limits.
+        # It makes a query cost less points and should not be used to production
         if self.name == "items":
-            params = {"query": f"query {{ boards ({graphql_query}) {{ {self.name}(limit:100){{ {self._get_query_by_stream_properties(self._get_schema_root_properties())} }} }} }}"}
-        else:
-            params = {"query": f"query {{ {self.name} ({graphql_query}) {{ {self._get_query_by_stream_properties(self._get_schema_root_properties())} }} }}"}
-        return params
+            return {"query": f"query {{ boards ({graphql_query}) {{ {self.name}(limit:100){{ {self._get_query_by_stream_properties(self._get_schema_root_properties())} }} }} }}"}
+
+        # Monday uses a query string to pass in environments
+        return {"query": f"query {{ {self.name} ({graphql_query}) {{ {self._get_query_by_stream_properties(self._get_schema_root_properties())} }} }}"}
--- airbyte-integrations/connectors/source-monday/unit_tests/test_graphql_request_options_provider.py
+++ airbyte-integrations/connectors/source-monday/unit_tests/test_graphql_request_options_provider.py
@@ -19,20 +19,21 @@ nested_schema = {
 }
 
 @pytest.mark.parametrize(
-    ("input_schema", "graphql_query"),
+    ("input_schema", "graphql_query", "stream_name"),
     [
         pytest.param(
             nested_schema,
-            {"query": "query { boards (limit:100,page:2) { items(limit:100){ key_a{key_a_1{key_a_2}},key_b,key_c } } }"},
+            {"query": "query { test_stream (limit:100,page:2) { key_a{key_a_1{key_a_2}},key_b,key_c } }"},
+            "test_stream",
             id="test_get_request_params_produces_graphql_string_for_items"
         )
     ]
 )
-def test_get_request_params(mocker, input_schema, graphql_query):
+def test_get_request_params(mocker, input_schema, graphql_query, stream_name):
     mocker.patch.object(GraphQLRequestOptionsProvider, "_get_schema_root_properties", return_value=input_schema)
     provider = GraphQLRequestOptionsProvider(
         limit="{{ options['items_per_page'] }}",
-        options={"name": "items", "items_per_page": 100}
+        options={"name": stream_name, "items_per_page": 100}
     )
     assert provider.get_request_params(
         stream_state={},
